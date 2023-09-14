7577e27c4807868e62a2f1b5eb5bf9e07be2886c
Fernando Robles <fernandorobles97@yahoo.com>
2023-09-14 13:02:11 -0500

    Add useEffect with apiCall; create states for loading and eror; pass articles to home

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import NavBar from '../NavBar/NavBar';
 import './App.css';
 import Home from '../Home/Home';
 import { Route, Routes } from 'react-router-dom';
 import ArticleDetails from '../ArticleDetails/ArticleDetails';
+import getAllArticles from '../../apiCalls';
+import newsData from '../../newsData.json'
 
 const App = () => {
   const [articles, setArticles] = useState([])
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const apiCall = async () => {
+      setLoading(true)
+      try {
+        setArticles(await getAllArticles())
+        setLoading(false)
+      }catch(error) {
+        setError(error)
+      }
+      setLoading(false)
+    } 
+    apiCall()
+  }, [])
 
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home articles={articles} />} />
         <Route path='/:articleID' element={<ArticleDetails />} />
       </Routes>
     </div>
