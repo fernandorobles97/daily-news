6539a08d548503a3d6f03885b8591646ddc955ba
Fernando Robles <fernandorobles97@yahoo.com>
2023-09-14 00:00:44 -0500

    Add routes to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+import NavBar from '../NavBar/NavBar';
 import './App.css';
-import logo from '../../images/logo.png'
+import Home from '../Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import ArticleDetails from '../ArticleDetails/ArticleDetails';
 
-function App() {
+const App = () => {
+  const [articles, setArticles] = useState([])
+
   return (
     <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:articleID' element={<ArticleDetails />} />
+      </Routes>
     </div>
   );
 }
