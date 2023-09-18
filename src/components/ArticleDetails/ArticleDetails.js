edd1705b71341349fdc6d0a672b90146cc883712
Fernando Robles <fernandorobles97@yahoo.com>
2023-09-17 21:14:32 -0500

    Remove console logs

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -4,12 +4,11 @@ import { useLocation } from 'react-router-dom'
 
 const ArticleDetails = ({ currentArticle, updateCurrentArticle }) => {
   const location = useLocation().pathname.slice(1).replaceAll('%20', ' ')
-  console.log(location)
-  console.log('cur', currentArticle)
 
   useEffect(() => {
     updateCurrentArticle(location)
   }, [currentArticle])
+  
   return (
   <div className='article-details'>
     <h1>{currentArticle.title}</h1>
