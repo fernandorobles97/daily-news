ba781ceea021832d65a82e94dbad92c5dbb3044f
Fernando Robles <fernandorobles97@yahoo.com>
2023-09-18 19:27:18 -0500

    Move formatDate to helperFunction file so it can be used in App component as well

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from 'react-router-dom';
 import ArticleDetails from '../ArticleDetails/ArticleDetails';
 import getAllArticles from '../../apiCalls';
 import newsData from '../../newsData.json'
+import formatDate from '../../helperFunctions';
 
 
 const App = () => {
@@ -26,7 +27,7 @@ const App = () => {
 
   const updateCurrentArticle = (title) => {
     let foundArticle = articles.find(article => article.title === title)
-    const newArticle = { ...foundArticle, source: foundArticle.source.name }
+    const newArticle = { ...foundArticle, source: foundArticle.source.name, publishedAt: formatDate(foundArticle.publishedAt) }
     setCurrentArticle(newArticle)
   }
 
--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -1,15 +1,8 @@
 import { Link } from 'react-router-dom'
 import './ArticleCard.css'
+import formatDate from '../../helperFunctions'
 
 const ArticleCard = ({ title, source, date, imgSrc }) => {
-  const formatDate = (data) => {
-    let month = data.slice(5, 7)
-    let day = data.slice(8, 10)
-    let year = data.slice(0, 4)
-
-    return `${month}/${day}/${year}`
-  }
-
   return (
     <Link to={`/${title}`} className='article-card' key={title} >
       <div className='card-details' >
