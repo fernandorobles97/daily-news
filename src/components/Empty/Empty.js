0fcb2ac51a15c7d246c200fc7911ba9aaf0453ee
Fernando Robles <fernandorobles97@yahoo.com>
2023-09-21 12:43:54 -0500

    'Add Empty component and styling rules for it

---
+++ src/components/Empty/Empty.js
@@ -0,0 +1,11 @@
+import './Empty.css'
+
+const Empty = () => {
+  return (
+    <div className='empty-page'>
+      <h1>This page does not exist, please go back!</h1>
+    </div>
+  )
+}
+
+export default Empty
