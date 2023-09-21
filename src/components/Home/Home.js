539a6e19d1628398cc3e3bc641167d9d329f7da8
Fernando Robles <fernandorobles97@yahoo.com>
2023-09-21 15:42:53 -0500

    Add conditional render based on if there are articles yet

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,18 @@
 import { useEffect, useState } from 'react'
 import './Home.css'
 import CardContainer from '../CardContainer/CardContainer'
+import Empty from '../Empty/Empty'
 
 const Home = ({ articles, filteredArticles, searching }) => {
   return (
-    <section className='home'>
-      <div className='header-sort-container'>
-        {searching ?<h1>Articles Based On Search</h1> : <h1>All Articles</h1> }
-      </div>
-      {searching ? <CardContainer articlesToDisplay={filteredArticles} /> : <CardContainer articlesToDisplay={articles} /> }
-    </section>
+    <>    
+      {articles.length ? <section className='home'>  
+        <div className='header-sort-container'>
+          {searching ? <h1>Articles Based On Search</h1> : <h1>All Articles</h1> }
+        </div>
+        { searching ? <CardContainer articlesToDisplay={filteredArticles} /> : <CardContainer articlesToDisplay={articles} /> }
+      </section>: <Empty />}
+    </>
   )
 }
 
