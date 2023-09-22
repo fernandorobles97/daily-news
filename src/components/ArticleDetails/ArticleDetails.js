38db05b25edfaee5e36db95187b70441c4bd6845
Fernando Robles <fernandorobles97@yahoo.com>
2023-09-21 23:18:30 -0500

    Add link to article at end of description

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import './ArticleDetails.css'
-import { useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import Empty from '../Empty/Empty'
 
 const ArticleDetails = ({ currentArticle, updateCurrentArticle }) => {
@@ -26,7 +26,7 @@ const ArticleDetails = ({ currentArticle, updateCurrentArticle }) => {
       </div>
       <div className='description-container'>
         <h2>Description</h2>
-        <p className='article-description'>{currentArticle.content}</p>
+        <p className='article-description'>{currentArticle.content.slice(0, 200)} <Link to={currentArticle.url} className='article-link'>Click here to read more!</Link></p>
       </div>
       <img src={currentArticle.urlToImage} />
       <p>Published on {currentArticle.publishedAt}</p>
